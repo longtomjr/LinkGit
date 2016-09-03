bc2d5496f022782e593a45d528097fe125d3421f
longtomjr <longtomjr@gmail.com>
2016-09-03 13:24:23 +0200

    Beautifully and masterfully made the code better and removed all the events out of the main program :)

---
+++ LinkGit/DiscordControler.cs
@@ -0,0 +1,102 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DiscordControler.cs">
+//    This file is part of LinkGit.
+//
+//    LinkGit is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    LinkGit is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LinkGit.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace LinkGit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Discord;
+    using System.Text.RegularExpressions;
+
+    public class DiscordControler
+    {
+        private DiscordClient _client;
+        public bool Logging;
+        public bool Linking;
+        private SQLiteController _dbcontroler;
+
+        public DiscordControler(bool logging = true, bool linking = true)
+        {
+            this._dbcontroler = new SQLiteController();
+            this._dbcontroler.OpenDB(@"./Chatlog");
+            this._client = new DiscordClient();
+            this.Logging = logging;
+            this.Linking = linking;
+
+            this._client.MessageReceived += this._client_MessageReceived;
+
+            this._client.MessageDeleted += this._client_MessageDeleted;
+
+            this._client.MessageUpdated += this._client_MessageUpdated;
+
+            string token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+            if (token != null)
+            {
+                this._client.ExecuteAndWait(async () =>
+                {
+                    await _client.Connect(token);
+                });
+            }
+            else
+            {
+                this._client.ExecuteAndWait(async () =>
+                {
+                    await _client.Connect(System.IO.File.ReadAllText(@"./token"));
+                });
+            }
+        }
+
+        private void _client_MessageUpdated(object sender, MessageUpdatedEventArgs e)
+        {
+            _dbcontroler.EditMessage(e.Before.Id, e.After.Text);
+        }
+
+        private void _client_MessageDeleted(object sender, MessageEventArgs e)
+        {
+            _dbcontroler.RemoveMessage(e.Message.Id);
+        }
+
+        async private void _client_MessageReceived(object sender, MessageEventArgs e)
+        {                
+            // Checks the message to see if there is a #[number] in the message
+            Match m = Regex.Match(e.Message.Text, "#(\\d+)");
+            if (!e.Message.IsAuthor && m.Success && this.Logging)
+            {     
+                // create a url for the issue page (removes the '#')
+                string page = "https://github.com/TeamPorcupine/ProjectPorcupine/issues/" + m.ToString().Substring(1);
+
+                // Checks if the page exists
+                using (HttpClient pageChecker = new HttpClient())
+                {
+                    var response = await pageChecker.GetAsync(page);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        await e.Channel.SendMessage(page);
+                    }
+                }
+               
+            }
+            this._dbcontroler.AddMessage(e.Message.Id, e.Message.User.ToString(), e.Message.Text, e.Message.Timestamp, e.Channel.Id, e.Channel.Name);
+        }
+    }
+}
--- LinkGit/Program.cs
+++ LinkGit/Program.cs
@@ -32,7 +32,7 @@ namespace LinkGit
     /// </summary>
     public class Program
     {
-        private DiscordClient _client;
+        private DiscordControler _dcControler;
 
         /// <summary>
         /// Calls the Start function when running the program
@@ -45,66 +45,8 @@ namespace LinkGit
         /// </summary>
         public void Start()
         {
-            var dbcontroler = new SQLiteController();
-            dbcontroler.OpenDB(@"./Chatlog");
+            this._dcControler = new DiscordControler();
 
-            this._client = new DiscordClient();
-
-            // Checks for event MessageReceived
-            this._client.MessageReceived += async (s, e) =>
-            {
-                // Checks that the bot is not the author (prevents echo loop)
-                if (!e.Message.IsAuthor)
-                {
-                    // Checks the message to see if there is a #[number] in the message
-                    Match m = Regex.Match(e.Message.Text, "#(\\d+)");
-                    if (m.Success)
-                    {
-                        // create a url for the issue page (removes the '#')
-                        string page = "https://github.com/TeamPorcupine/ProjectPorcupine/issues/" + m.ToString().Substring(1);
-
-                        // Checks if the page exists
-                        using (HttpClient pageChecker = new HttpClient())
-                        {
-                            var response = await pageChecker.GetAsync(page);
-
-                            if (response.IsSuccessStatusCode)
-                            {
-                                await e.Channel.SendMessage(page);
-                            }
-                        }
-                    }
-                }
-
-                dbcontroler.AddMessage(e.Message.Id, e.Message.User.ToString(), e.Message.Text, e.Message.Timestamp, e.Channel.Id, e.Channel.Name);
-            };
-
-            dbcontroler.PrintMessages(221343758124449802, 221344145011113984, 216284510692704276);
-
-            this._client.MessageDeleted += (s, e) =>
-            {
-                dbcontroler.RemoveMessage(e.Message.Id);
-            };
-
-            this._client.MessageUpdated += (s, e) =>
-            {
-                dbcontroler.EditMessage(e.Before.Id, e.After.Text);
-            };
-             string token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
-            if (token != null)
-            {
-                this._client.ExecuteAndWait(async () => 
-                {
-                    await _client.Connect(token);
-                });
-            }
-            else
-            {
-                this._client.ExecuteAndWait(async () => 
-                {
-                    await _client.Connect(System.IO.File.ReadAllText(@"./token"));
-                });
-            }
         }
     }
 }
